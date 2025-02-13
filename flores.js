62c4a1a6ceb9f70fe551727b90db2277bac22426
Roberth Chiquito <rchiquito07@gmail.com>
2025-02-13 17:09:52 -0500

    upgrade flores

--- flores.js
+++ flores.js
@@ -17,3 +17,6 @@ const generateSpaceLayer = (size, selector, totalStars, duration) => {
 generateSpaceLayer("2px", ".space-1", 250, "25s");
 generateSpaceLayer("3px", ".space-2", 100, "20s");
 generateSpaceLayer("6px", ".space-3", 25, "15s");
+onload = () => {
+  document.body.classList.remove("container");
+};
